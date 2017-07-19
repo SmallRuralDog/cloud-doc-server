9ead03b426b32e3cd73bcf9de31df1db5e123a53
ZhangWei <296404875@qq.com>
2017-07-19 20:17:37 +0800

    123

--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'cloud-doc'),
+            'database' => env('DB_DATABASE', 'cloud_doc'),
             'username' => env('DB_USERNAME', 'cloud_doc'),
             'password' => env('DB_PASSWORD', '5cb3Z2Ymzt'),
             'unix_socket' => env('DB_SOCKET', ''),
