9c7bd7459d36958b44fc0c16f3d9c5301f76b74a
ZhangWei <296404875@qq.com>
2017-07-20 12:34:04 +0800

    123

--- app/Admin/Controllers/DocPageController.php
+++ app/Admin/Controllers/DocPageController.php
@@ -132,14 +132,14 @@ class DocPageController extends Controller
                 $form->content = str_replace("<?php","",$form->content);
                 $form->content = str_replace("{tip}","",$form->content);
                 $form->content = str_replace("{note}","",$form->content);
-                $form->content = preg_replace("/<a name=\"(.*)\"><\/a>/is", "", $form->content);
+                $form->content = preg_replace("/<a[^>]*><\/a>/is", "", $form->content);
             });
 
             $form->saved(function ($form) {
                 $form->content = str_replace("<?php","",$form->content);
                 $form->content = str_replace("{tip}","",$form->content);
                 $form->content = str_replace("{note}","",$form->content);
-                $form->content = preg_replace("/<a name=\"(.*)\"><\/a>/is", "", $form->content);
+                $form->content = preg_replace("/<a[^>]*><\/a>/is", "", $form->content);
                 // 跳转页面
                 return redirect(admin_url("doc-page?menu_id=" . $form->menu_id."&doc_id=".$form->doc_id));
             });
