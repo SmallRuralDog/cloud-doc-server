c38e6f9f46c5639921907bcaeffc523e2a603ba7
ZhangWei <296404875@qq.com>
2017-07-21 15:43:42 +0800

    123

--- app/Http/Controllers/Api/DocController.php
+++ app/Http/Controllers/Api/DocController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Extend\Parsedown;
 use App\Http\Controllers\Controller;
 use App\Models\Doc;
 use App\Models\DocMenu;
 use App\Models\DocPage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 /**
  * Created by PhpStorm.
@@ -20,8 +20,11 @@ class DocController extends Controller
 
     public function lists()
     {
-        $doc = Doc::query()->where("state",1);
-        $list = $doc->paginate(50, ['id', 'title', 'desc', 'cover']);
+        $list = Cache::remember('doc_index', 10, function () {
+            $doc = Doc::query()->where("state", 1);
+            $list = $doc->paginate(50, ['id', 'title', 'desc', 'cover']);
+            return $list;
+        });
         return response()->json($list);
     }
 
@@ -33,20 +36,22 @@ class DocController extends Controller
         return response()->json(['data' => $doc, 'message' => '', 'status_code' => 1]);
     }
 
-    public function menu(Request $request){
+    public function menu(Request $request)
+    {
         $doc_id = $request->input("doc_id");
-        $doc_menu = DocMenu::query()->where("doc_id",$doc_id);
-        $list = $doc_menu->get(['id','title']);
-        foreach ($list as $k=>$v){
-            $list[$k]->page = $v->doc_page()->orderBy("order","asc")->orderBy("id","asc")->get(['id','menu_title','menu_id']);
+        $doc_menu = DocMenu::query()->where("doc_id", $doc_id);
+        $list = $doc_menu->get(['id', 'title']);
+        foreach ($list as $k => $v) {
+            $list[$k]->page = $v->doc_page()->orderBy("order", "asc")->orderBy("id", "asc")->get(['id', 'menu_title', 'menu_id']);
         }
         return response()->json(['data' => $list, 'message' => '', 'status_code' => 1]);
     }
 
-    public function page(Request $request){
+    public function page(Request $request)
+    {
         $page_id = $request->input("page_id");
 
-        $page = DocPage::query()->find($page_id,['content','updated_at']);
+        $page = DocPage::query()->find($page_id, ['content', 'updated_at']);
         //$Parsedown = new Parsedown();
         //$page->content = $Parsedown->text($page->content);
 
