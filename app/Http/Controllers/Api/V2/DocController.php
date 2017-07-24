ef472ee799072abce1ade3e9028a1deb06bcf940
ZhangWei <296404875@qq.com>
2017-07-24 11:31:42 +0800

    123

--- app/Http/Controllers/Api/V2/DocController.php
+++ app/Http/Controllers/Api/V2/DocController.php
@@ -27,7 +27,7 @@ class DocController extends Controller
     {
 
         $class_id = $request->input("class_id");
-        $doc = Doc::query()->where("state", 1)->orderBy("order", "desc")->orderBy("id");
+        $doc = Doc::query()->where("state",">=", 0)->orderBy("order", "desc")->orderBy("id");
         $doc->where("doc_class_id", $class_id);
         $list = $doc->paginate(20, ['id', 'title', 'desc', 'cover','doc_class_id']);
 
