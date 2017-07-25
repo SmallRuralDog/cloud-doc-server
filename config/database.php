5c9e919e7a74e2fdc6a53f6f84316d9d532ef6a7
ZhangWei <296404875@qq.com>
2017-07-25 10:12:13 +0800

    123

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'rm-bp18fe7avy5r1567l.mysql.rds.aliyuncs.com'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'cloud_doc'),
             'username' => env('DB_USERNAME', 'cloud_doc'),
-            'password' => env('DB_PASSWORD', 'Zw1992zw1118'),
+            'password' => env('DB_PASSWORD', '5cb3Z2Ymzt'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
