f6fa906e6d2651747355310643c34cc2d0373a36
ZhangWei <296404875@qq.com>
2017-08-08 17:10:39 +0800

    123

--- app/Http/Controllers/Api/V3/ArticleController.php
+++ app/Http/Controllers/Api/V3/ArticleController.php
@@ -12,11 +12,13 @@ namespace App\Http\Controllers\Api\V3;
 use App\Http\Controllers\Controller;
 use App\Models\Ad;
 use App\Models\Article;
+use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $page = $request->input("page", 1);
         $article = Article::query();
 
         $list = $article->paginate(20, ['id', 'title', 'desc', 'cover', 'view_count']);
@@ -26,6 +28,15 @@ class ArticleController extends Controller
         }
         $swiper = Ad::query()->where('loca_id', 1)
             ->where('state', 1)->orderBy('order', 'desc')->limit(5)->get(['id', 'title', 'page', 'cover', 'loca_id', 'open_type']);
+
+        if ($page == 1) {
+            $json = json_encode($list);
+
+            $list = json_decode($json);
+
+            $list->swiper = $swiper;
+        }
+
 
         return response()->json($list);
     }
