06f4314f2770e4bceab9133b2db387b29b6347ca
ZhangWei <296404875@qq.com>
2017-08-09 09:43:37 +0800

    123

--- app/Http/Controllers/Api/V3/ArticleController.php
+++ app/Http/Controllers/Api/V3/ArticleController.php
@@ -12,6 +12,7 @@ namespace App\Http\Controllers\Api\V3;
 use App\Http\Controllers\Controller;
 use App\Models\Ad;
 use App\Models\Article;
+use App\Models\ArticleTag;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -19,7 +20,15 @@ class ArticleController extends Controller
     public function index(Request $request)
     {
         $page = $request->input("page", 1);
+        $tag_id = $request->input("tag_id", 0);
         $article = Article::query();
+        if ($tag_id > 0) {
+            $article_ids = ArticleTag::query()->where('tag_id', $tag_id)->get(['article_id'])->pluck('article_id')->toArray();
+            if (is_array($article_ids)) {
+                $article->whereIn('id', $article_ids);
+            }
+        }
+
 
         $list = $article->paginate(20, ['id', 'title', 'desc', 'cover', 'view_count']);
 
@@ -29,7 +38,7 @@ class ArticleController extends Controller
         $swiper = Ad::query()->where('loca_id', 3)
             ->where('state', 1)->orderBy('order', 'desc')->limit(5)->get(['id', 'title', 'page', 'cover', 'loca_id', 'open_type']);
 
-        if ($page == 1) {
+        if ($page == 1 && $tag_id == 0) {
             $json = json_encode($list);
 
             $list = json_decode($json);
@@ -45,9 +54,8 @@ class ArticleController extends Controller
     {
         $id = $request->input("id");
 
-        $page = Article::query()->find($id, ['id','title','content', 'updated_at']);
+        $page = Article::query()->find($id, ['id', 'title', 'content', 'updated_at']);
         $page->increment("view_count");
-
 
 
         return response()->json(['data' => $page, 'message' => '', 'status_code' => 1]);
