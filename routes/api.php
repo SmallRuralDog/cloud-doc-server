74de04519d801437110aac52b9f0b65d58c6311e
ZhangWei <296404875@qq.com>
2017-08-11 16:49:16 +0800

    123

--- routes/api.php
+++ routes/api.php
@@ -43,6 +43,7 @@ $api->version(['v1', 'v2'], function (Dingo\Api\Routing\Router $api) {
 
         $api->group(['middleware' => ['before' => 'jwt.auth']], function (Dingo\Api\Routing\Router $api) {
             $api->get('user-index','UserController@index');
+            $api->get('user-follow','UserController@user_follow');
         });
     });
 
