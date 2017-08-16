e92e84f2e104f816506ba24e9caa6e41960d0e88
ZhangWei <296404875@qq.com>
2017-08-16 19:00:13 +0800

    123

--- app/Http/Controllers/Api/V3/QuestionController.php
+++ app/Http/Controllers/Api/V3/QuestionController.php
@@ -47,7 +47,7 @@ class QuestionController extends BaseController
         $id = $request->input("id");
         $page = $request->input("page", 1);
 
-        $v = Question::query()->find($id, ['id', 'user_id', 'title', 'desc', 'pics', 'created_at', 'view_count']);
+        $v = Question::query()->find($id, ['id', 'user_id', 'title', 'desc', 'pics', 'created_at', 'view_count','source','source_id']);
         $v->user = $v->user()->first(['id', 'name', 'title', 'avatar']);
 
         $v->created = Carbon::parse($v->created_at)->diffForHumans();
